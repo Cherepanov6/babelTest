dc941f0e9d243989fabc2bd08e0c2efeacfd8031
cherepanov_vs <cherepanov_vs@ursip.ru>
2018-04-26 17:30:19 +0300

    Промежуточный

---
+++ es2015.js
@@ -0,0 +1,4 @@
+let a = `kaka ${ 1+14 } buka`;
+const ka = (oldka) => oldka;
+const b = (ka) => console.log('%s', ka);
+
---
+++ es5.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var a = 'kaka ' + (1 + 14) + ' buka';
+var ka = function ka(oldka) {
+  return oldka;
+};
+var b = function b(ka) {
+  return console.log('%s', ka);
+};
